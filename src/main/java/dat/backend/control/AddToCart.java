66ee92091b8630c2254aef01723bb3de5611f5fb
Johan M <cph-jm390@cphbusiness.dk>
2022-11-11 13:27:03 +0100

    upgrade til pris + top/bund navne i ordrelinjen

--- src/main/java/dat/backend/control/AddToCart.java
+++ src/main/java/dat/backend/control/AddToCart.java
@@ -9,9 +9,12 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "AddToCart", value = "/addtocart")
 public class AddToCart extends HttpServlet {
+    int totalPris;
     ConnectionPool connectionPool = new ConnectionPool();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -22,17 +25,23 @@ public class AddToCart extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         ShoppingCart cart= (ShoppingCart) session.getAttribute("cart");
-
-        int idTopping = Integer.parseInt(request.getParameter("topping"));
-        int topPris = CupcakeFacade.getTopping(connectionPool).get(idTopping-1).getToppingPrice();
-
+       List<String> cupcakeNames = new ArrayList<>();
+    int idTopping = Integer.parseInt(request.getParameter("topping"));
     int idBottom = Integer.parseInt(request.getParameter("bottom"));
-    int botPris = CupcakeFacade.getBottom(connectionPool).get(idBottom-1).getBottomPrice();
     int quantity = Integer.parseInt(request.getParameter("quantity"));
-    int totalPris= (botPris+topPris)*quantity;
+    int topPris = CupcakeFacade.getTopping(connectionPool).get(idTopping-1).getToppingPrice()*quantity;
+    int botPris = CupcakeFacade.getBottom(connectionPool).get(idBottom-1).getBottomPrice()*quantity;
+    totalPris+=topPris+botPris;
+    String toppingVar=CupcakeFacade.getTopping(connectionPool).get(idTopping).getToppingVar();
+    String bottomVar=CupcakeFacade.getBottom(connectionPool).get(idBottom).getBottomVar();
+    cupcakeNames.add(toppingVar);
+    cupcakeNames.add(bottomVar);
+
+
     Cupcake cupcake = new Cupcake(idTopping,idBottom,quantity);
     cart.add(cupcake);
     session.setAttribute("cart", cart);
+        session.setAttribute("cupcakeNames", cupcakeNames);
     request.setAttribute("cartsize", cart.getNumberOfCupcakes());
         request.setAttribute("totalPris", totalPris);
     request.getRequestDispatcher("WEB-INF/welcome.jsp").forward(request,response)   ;
