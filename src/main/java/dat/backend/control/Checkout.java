ddb48e479c13b12f2b688c45cd7b04e751328003
Misfosster <williamfossnielsen@hotmail.com>
2022-11-12 13:28:36 +0100

    Checkout + save to db fufu

---
+++ src/main/java/dat/backend/control/Checkout.java
@@ -0,0 +1,33 @@
+package dat.backend.control;
+
+import dat.backend.model.entities.ShoppingCart;
+import dat.backend.model.persistence.CupcakeMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "Checkout", value = "/checkout")
+public class Checkout extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
+        ArrayList<Integer> priceList = (ArrayList<Integer>) session.getAttribute("priceList");
+        CupcakeMapper cupcakeMapper = (CupcakeMapper) session.getAttribute("mapper");
+
+        cupcakeMapper.insertCupcakeToDB(cart, priceList);
+        request.getRequestDispatcher("welcome.jsp").forward(request, response);
+    }
+}
