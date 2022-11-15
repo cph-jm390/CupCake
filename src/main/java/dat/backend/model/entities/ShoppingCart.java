81f99077c0426c264cebf2aae5efff7659e89438
Misfosster <williamfossnielsen@hotmail.com>
2022-11-15 13:14:53 +0100

    ShoppingcartID delt til cupcakes, samt oprettet shoppingcart

--- src/main/java/dat/backend/model/entities/ShoppingCart.java
+++ src/main/java/dat/backend/model/entities/ShoppingCart.java
@@ -6,8 +6,41 @@ import java.util.List;
 public class ShoppingCart {
 
     List<Cupcake> cupcakeList = new ArrayList<>();
+    String username;
+    String status;
+    int idShoppingcart;
+
 
     public ShoppingCart() {
+    }
+
+    public ShoppingCart(String username, String status, int idShoppingcart) {
+        this.username = username;
+        this.status = status;
+        this.idShoppingcart = idShoppingcart;
+    }
+    public int getIdShoppingcart() {
+        return idShoppingcart;
+    }
+
+    public void setIdShoppingcart(int idShoppingcart) {
+        this.idShoppingcart = idShoppingcart;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     public void clearShoppingcart()
